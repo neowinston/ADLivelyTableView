004815fe42c8807cc8aa992dcd4d18e736af8ac5
neowinston <mariosergio7@gmail.com>
2019-03-21 16:15:51 -0400

    Update LDAppDelegate.h

--- Demo/Classes/LDAppDelegate.h
+++ Demo/Classes/LDAppDelegate.h
@@ -11,7 +11,6 @@
 @interface LDAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 @property (strong, nonatomic) UINavigationController *navigationController;
 
 @end
